ed958194f61c228db2fc88d427110ce91b181340
fedor <ahmedalbachiry@gmail.com>
2021-05-02 23:48:52 +0300

    fix close futures marcket  position

--- testbot/app.js
+++ testbot/app.js
@@ -1,6 +1,4 @@
 const Binance = require('node-binance-api');
-const apiKey = "2k82QayDqeL36edeO3kkUPFTQrqGyICzPYRmw2cC3BHsSjteaIE0k2dcuGoyY4dU"
-const secretKey = "tpwei5t7NOdvYJx1qhKlQ3VqZincxuo6NVg4jdGmo2kMrmhkwcvYoDrdeWp1mpJz"
 
 const binance = new Binance().options({
   APIKEY: apiKey,
@@ -11,24 +9,30 @@ async function prcs() {
     console.info( await binance.futuresPrices() );
 }
 //prcs()
+
 async function balance() {
     console.info( await binance.futuresBalance() );
 }
 //balance()
-
+let resp
 async function marketSell() {
-    console.info( await binance.futuresMarketSell( 'ONEUSDT', 100 ) );
+  resp = await binance.futuresMarketSell( 'ONEUSDT', 100 ) 
+    console.info(resp);
 }
+//marketSell()
 
-async function test() {
-    let position_data = await binance.futuresPositionRisk(), markets = Object.keys( position_data );
-    for ( let market of markets ) {
-      let obj = position_data[market], size = Number( obj.positionAmt );
-      if ( size == 0 ) continue;
-      console.info( `${leverage}x\t${market}\t${obj.unRealizedProfit}` );
-    }
+async function close() {
+    if ( resp.side == 'LONG' ) order = await binance.futuresMarketSell( 'ONEUSDT', 100, {reduceOnly: true} )
+    else order = await binance.futuresMarketBuy( 'ONEUSDT', 100, {reduceOnly: true} )
+    console.log(order)
 }
-marketSell()
+//setTimeout(function(){close()}, 5000)
 
-setTimeout(function(){test()}, 1000)
-
+// =========================================
+// ======== SPOT API ======================
+async function spotPrice() {
+    console.log("start spot")
+    let ticker = await binance.prices();
+    console.info(`spot Price : ${ticker.ONEUSDT}`);
+}
+spotPrice()
