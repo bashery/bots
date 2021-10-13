e9086f1c3cba796aa4730789b012b2c56d08d458
bashery <ahmedalbachiry@gmail.com>
2021-10-13 16:30:25 +0300

    reuse & rename this repo

--- testbot/websock.js
+++
@@ -1,8 +0,0 @@
-const WebSocket = require('ws');
-
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/oneusdt@trade');
-
-ws.on('message', function incoming(json) {
-    let data = JSON.parse(json);
-    console.log(data.p);
-});
